7d17008b862abe01be78b7d6507c58f22811f2b8
PrivateGER <privateger@privateger.me>
2021-03-09 19:44:56 +0100

    New files

--- encrypt.sh
+++ encrypt.sh
@@ -3,5 +3,5 @@
 DIRECTORY=.
 
 for i in $DIRECTORY/*.txt; do
-    gpg --yes -r privateger@tutanota.com --sign --armor --encrypt $i
+    gpg --yes --local-user privateger@privateger.me -r privateger@privateger.me --sign --armor --encrypt $i
 done
